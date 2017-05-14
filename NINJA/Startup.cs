11715de049e29dbbfd63e37467cc8621ec25247d
emmaielle <moira.lasserre@gmail.com>
2017-05-14 15:42:38 -0300

    Add project files.

---
+++ NINJA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NINJA.Startup))]
+namespace NINJA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
